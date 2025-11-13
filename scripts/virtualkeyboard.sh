a02daf4953015a4c6278f783ded8229b623dfea1
Szybet <53944559+Szybet@users.noreply.github.com>
2025-11-13 23:09:50 +0100

    virtual keyboard

---
+++ scripts/virtualkeyboard.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SQUEEKBOARD_PID=$(pgrep squeekboard-restyled)
+SHOWN_FLAG="/tmp/squeekboard_shown"
+
+if [ -z "$SQUEEKBOARD_PID" ]; then
+    # squeekboard is not running, start it and set shown flag
+    squeekboard-restyled &
+    touch "$SHOWN_FLAG"
+else
+    # squeekboard is running
+    if [ -f "$SHOWN_FLAG" ]; then
+        # It's shown, so hide it
+        busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b false
+        rm "$SHOWN_FLAG"
+    else
+        # It's not shown, so show it
+        busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b true
+        touch "$SHOWN_FLAG"
+    fi
+fi
