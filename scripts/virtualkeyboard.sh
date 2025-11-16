1f88a3b36cafb66261c57e9f18d84373a9271ce0
Szybet <53944559+Szybet@users.noreply.github.com>
2025-11-16 12:50:37 +0100

    keyboard improvement

--- scripts/virtualkeyboard.sh
+++ scripts/virtualkeyboard.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-SQUEEKBOARD_PID=$(pgrep squeekboard)
-SHOWN_FLAG="/tmp/squeekboard_shown"
-
-if [ -z "$SQUEEKBOARD_PID" ]; then
-    # squeekboard is not running, start it and set shown flag
-    squeekboard-restyled &
-    touch "$SHOWN_FLAG"
+CURRENT_STATE=$(busctl get-property --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 Visible)
+if [ "$CURRENT_STATE" == "b true" ]; then
+    busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b false
 else
-    # squeekboard is running
-    if [ -f "$SHOWN_FLAG" ]; then
-        # It's shown, so hide it
-        busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b false
-        rm "$SHOWN_FLAG"
-    else
-        # It's not shown, so show it
-        busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b true
-        touch "$SHOWN_FLAG"
-    fi
+    busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b true
 fi
+
