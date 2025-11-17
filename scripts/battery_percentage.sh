fa72f3017d862559ed3502034e2fc8f356325310
Szybet <53944559+Szybet@users.noreply.github.com>
2025-11-17 17:15:33 +0100

    remove scripts

--- scripts/battery_percentage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-percentage=$(upower -i $(upower -e | grep 'battery') 2>/dev/null | grep "percentage" | awk '{print $2}' | sed 's/%//')
-
-if [ -z "$percentage" ]; then
-    percentage=0
-fi
-
-echo "$percentage"
--- scripts/battery_state.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-state=$(upower -i $(upower -e | grep 'battery') 2>/dev/null | grep "state" | awk '{print $2}')
-
-if [ -z "$state" ]; then
-    state="Unknown"
-fi
-
-echo "$state"
