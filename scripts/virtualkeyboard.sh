4a41e37aee8abcf87b2ced988eac7547bd10ccb6
Szybet <53944559+Szybet@users.noreply.github.com>
2025-11-17 17:35:30 +0100

    keyboard

--- scripts/virtualkeyboard.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CURRENT_STATE=$(busctl get-property --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 Visible)
-if [ "$CURRENT_STATE" == "b true" ]; then
-    busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b false
-else
-    busctl call --user sm.puri.OSK0 /sm/puri/OSK0 sm.puri.OSK0 SetVisible b true
-fi
-
